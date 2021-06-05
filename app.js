88213fb592893690c6ea7f222335da00a3b234b6
Natalia Szlapka <n.machalica@gmail.com>
2021-06-05 14:35:46 +0200

    Identity generator

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
-const os = require('os');
+const fs = require('fs');
 
-console.log('Platform: ', os.platform());
-console.log('Arch: ', os.arch());
-console.log('userInfo: ', os.userInfo());
+const genders = [ 'male', 'female' ];
+const maleNames = [ 'Leonard','James', 'Kirk', 'Ralph', 'Frank', 'David', 'Alex', 'Kriss', 'James', 'Harry' ];
+const femaleNames = [ 'Viola', 'Nena', 'Emily', 'Kim', 'Olivia', 'Sarah', 'Mary', 'Jane', 'Laura', 'Karen' ];
+const lastNames = [ 'Smith', 'Brown', 'Davis', 'Miller', 'Jones', 'Parker', 'Williams', 'Griffin', 'Lopez', 'Wilson' ];
+const ages = [ 19, 22, 24, 27, 28, 34, 14, 16 ];
+
+const randChoice = (arr) => {
+	return arr[Math.floor(Math.random() * arr.length)]
+};
+
+const people = [];
+
+for(let i = 0; i < 20; i++) {
+	let person = {};
+
+	person.gender = randChoice(genders);
+
+	if(person.gender === 'female') {
+		person.name = randChoice(femaleNames);
+	} else {
+		person.name = randChoice(maleNames);
+	}
+
+	person.lastName = lastNames[Math.floor(Math.random() * lastNames.length)]
+
+	person.age = randChoice(ages);
+
+	people.push(person);
+
+};
+
+const dataArr = JSON.stringify(people);
+
+fs.writeFile('people.json', dataArr, (err) => {
+	if (err) throw err;
+	console.log('The file has been saved!');
+});
